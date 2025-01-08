087b901e51b993d81b9ce6230530a5a03248ee18
huberrt5671 <hubernotis34@gmail.com>
2025-01-08 11:45:49 +0900

    initial commit with folder structure and routing

---
+++ src/app/constants/index.js
@@ -0,0 +1,45 @@
+
+
+export const navLinks = [
+{
+   label: "Home", 
+   route:  "/",
+},
+{
+    label: "News", 
+    route:  "/",
+ },
+ {
+    label: "Sports", 
+    route:  "/",
+ },
+ {
+    label: "Business", 
+    route:  "/",
+ },
+ {
+    label: "Innovation", 
+    route:  "/",
+ },
+ {
+    label: "Culture", 
+    route:  "/",
+ },
+ {
+    label: "Arts", 
+    route:  "/",
+ },
+ {
+    label: "Travel", 
+    route:  "/",
+ },
+ {
+    label: "Earth", 
+    route:  "/",
+ },
+ {
+    label: "Weather", 
+    route:  "/",
+ },
+
+];
